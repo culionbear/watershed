ef7d9c720bc95f7544307fe7fcc16201ead4ecc5
root <2907990037@qq.com>
2022-12-04 13:46:09 +0800

    # 增加对module的config进行操作的方法

--- module/config.go
+++ module/config.go
@@ -2,15 +2,24 @@ package module
 
 import (
 	"errors"
+	"fmt"
+
 	consul "github.com/hashicorp/consul/api"
 )
 
 func (m *Module) GetConfig(key string) ([]byte, error) {
-	rsp, _, err := m.handler.KV().Get(key, nil)
+	rsp, _, err := m.handler.KV().Get(key, &consul.QueryOptions{
+		Token: m.conf.ConsulConfig.Token,
+	})
 	if err != nil {
 		return nil, err
 	}
 	return rsp.Value, nil
+}
+
+func (m *Module) GetModuleConfig(key string) ([]byte, error) {
+	key = fmt.Sprintf("%s.%s", m.path, key)
+	return m.GetConfig(key)
 }
 
 func (m *Module) GetConfigWithParser(key string, parser int, value any, funcList ...func([]byte) ([]byte, error)) error {
@@ -30,12 +39,24 @@ func (m *Module) GetConfigWithParser(key string, parser int, value any, funcList
 	return parsers[parser].Unmarshal(data, value)
 }
 
+func (m *Module) GetModuleConfigWithParser(key string, parser int, value any, funcList ...func([]byte) ([]byte, error)) error {
+	key = fmt.Sprintf("%s.%s", m.path, key)
+	return m.GetConfigWithParser(key, parser, value, funcList...)
+}
+
 func (m *Module) SetConfig(key string, value []byte) error {
 	_, err := m.handler.KV().Put(&consul.KVPair{
 		Key:   key,
 		Value: value,
-	}, nil)
+	}, &consul.WriteOptions{
+		Token: m.conf.ConsulConfig.Token,
+	})
 	return err
+}
+
+func (m *Module) SetModuleConfig(key string, value []byte) error {
+	key = fmt.Sprintf("%s.%s", m.path, key)
+	return m.SetConfig(key, value)
 }
 
 func (m *Module) SetConfigWithParser(key string, parser int, value any, funcList ...func([]byte) ([]byte, error)) error {
@@ -52,9 +73,10 @@ func (m *Module) SetConfigWithParser(key string, parser int, value any, funcList
 			return err
 		}
 	}
-	_, err = m.handler.KV().Put(&consul.KVPair{
-		Key:   key,
-		Value: data,
-	}, nil)
-	return err
+	return m.SetConfig(key, data)
+}
+
+func (m *Module) SetModuleConfigWithParser(key string, parser int, value any, funcList ...func([]byte) ([]byte, error)) error {
+	key = fmt.Sprintf("%s.%s", m.path, key)
+	return m.SetConfigWithParser(key, parser, value, funcList...)
 }
